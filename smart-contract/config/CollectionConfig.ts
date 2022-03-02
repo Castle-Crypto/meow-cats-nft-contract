6762fdafa60a6e4f0e59297a0f28bbfad8f105d1
kryptobaseddev <kryptokeaton@gmail.com>
2022-03-01 16:30:56 -0800

    initial update after deploy

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -5,24 +5,24 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
+  contractName: 'MeowCatsNFT',
+  tokenName: 'MeowCatsNFT',
+  tokenSymbol: 'MeowCatsNFT',
   hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  maxSupply: 5555,
   whitelistSale: {
-    price: 0.05,
+    price: 0.01,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
+    price: 0.01,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
+    price: 0.01,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
+  contractAddress: "0x2dCa84b546002FB6Bd6851BE396fe3f61b88b98A",
   openSeaSlug: 'my-nft-token',
   whitelistAddresses: whitelistAddresses,
 };
--- smart-contract/contracts/MeowCatsNFT.sol
+++ smart-contract/contracts/MeowCatsNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract YourNftToken is ERC721, Ownable, ReentrancyGuard {
+contract MeowCatsNFT is ERC721, Ownable, ReentrancyGuard {
 
   using Strings for uint256;
   using Counters for Counters.Counter;
@@ -164,11 +164,7 @@ contract YourNftToken is ERC721, Ownable, ReentrancyGuard {
   }
 
   function withdraw() public onlyOwner nonReentrant {
-    // This will pay HashLips Lab Team 5% of the initial sale.
-    // By leaving the following lines as they are you will contribute to the
-    // development of tools like this and many others.
-    // =============================================================================
-    (bool hs, ) = payable(0x146FB9c3b2C13BA88c6945A759EbFa95127486F4).call{value: address(this).balance * 5 / 100}("");
+    (bool hs, ) = payable(0xcB21e4FA46B06716DD8617CB5E8B48d7418965D0).call{value: address(this).balance * 25 / 100}("");
     require(hs);
     // =============================================================================
 
