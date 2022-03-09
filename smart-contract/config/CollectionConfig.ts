dc2e1833bc6eada16535ae16f077ad5748bbf2bc
kryptobaseddev <kryptokeaton@gmail.com>
2022-03-08 21:39:12 -0800

    Update CollectionConfig.ts

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -11,16 +11,16 @@ const CollectionConfig: CollectionConfigInterface = {
   hiddenMetadataUri: 'ipfs://QmYkc1Jp8BZRnsa5kio6rpDLD19QPyMg5iSjPRrgA668ce/hidden.json',
   maxSupply: 5555,
   whitelistSale: {
-    price: 0.12,
-    maxMintAmountPerTx: 1,
+    price: 0.10,
+    maxMintAmountPerTx: 30,
   },
   preSale: {
     price: 0.12,
-    maxMintAmountPerTx: 2,
+    maxMintAmountPerTx: 50,
   },
   publicSale: {
     price: 0.12,
-    maxMintAmountPerTx: 20,
+    maxMintAmountPerTx: 50,
   },
   contractAddress: "0x2dCa84b546002FB6Bd6851BE396fe3f61b88b98A",
   openSeaSlug: 'MeowCatsNFT',
